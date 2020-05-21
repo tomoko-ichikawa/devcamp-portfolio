3bd6f774e41519ae3bcabf44f762d3c5e28a1102
tomokoichikawa <takator@yahoo.co.jp>
2020-05-21 19:57:31 +0900

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "乃木坂推しメン #{blog}",
+    body: "生田絵梨花"
+  )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "乃木坂4期生 #{skill}",
+    percent_utilized: 96
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "乃木坂推しメン２ #{portfolio_item}",
+    subtitle: "乃木坂4期生",
+    body: "遠藤さくら",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
+
+puts "9 portgolio created"
