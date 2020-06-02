f544c711d3d40edd170a85f9976808270837bc64
tomokoichikawa <takator@yahoo.co.jp>
2020-06-02 11:21:45 +0900

    Added nav bar and added default header content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SetSource
-  include CurrentUserConcern
+  include DefaultPageContent
+
+
+  # 調子が悪いので、application_controllerに記載
+  # include CurrentUserConcern
+
+  def current_user
+    super || guest_user
+  end
+
+  def guest_user
+    OpenStruct.new(name: "Guest User", 
+                  first_name: "Guest", 
+                  last_name: "User", 
+                  email: "guest@example.com"
+                  )
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  # def set_title
+  #   @page_title = "Devcamp Portfolio | My Portfolio Website"
+  #   @blogs_index_title = "My Portfolio Blog"
+  # end
 end
