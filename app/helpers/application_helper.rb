a7990f44816d8c4a7f93c5c8fe9f758d131f52d8
tomokoichikawa <takator@yahoo.co.jp>
2020-06-02 16:25:48 +0900

    Implemented view helper for auth links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,18 @@ module ApplicationHelper
   #   @page_title = "Devcamp Portfolio | My Portfolio Website"
   #   @blogs_index_title = "My Portfolio Blog"
   # end
+
+  def sample_helper
+    "<p>My helper</p>".html_safe
+  end
+
+  def login_helper
+    if current_user.is_a?(User)
+      link_to "Logout", destroy_user_session_path, method: :delete
+    else
+      (link_to "Register", new_user_registration_path) +
+      "<br>".html_safe +
+      (link_to "Login", new_user_session_path)
+    end
+  end
 end
