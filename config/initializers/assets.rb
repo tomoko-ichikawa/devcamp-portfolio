bb5e606e90823fc2a240a1a8a89cc8e97e579b9f
tomokoichikawa <takator@yahoo.co.jp>
2020-06-02 13:56:00 +0900

    Added custom layouts for blogs and portfolios

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+#記載なしでも動く
+# Rails.application.config.assets.precompile += %w( blogs.scss )
+# Rails.application.config.assets.precompile += %w( portfolios.scss )
