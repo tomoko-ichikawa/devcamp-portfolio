aa7f92820509fce15eeebc3a6e3a89924db71c36
tomokoichikawa <takator@yahoo.co.jp>
2020-06-03 11:22:46 +0900

    Implemented content tag helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,9 @@ module ApplicationHelper
   #   @blogs_index_title = "My Portfolio Blog"
   # end
 
-  def sample_helper
-    "<p>My helper</p>".html_safe
-  end
+  # def sample_helper
+  #   "<p>My helper</p>".html_safe
+  # end
 
   def login_helper
     if current_user.is_a?(User)
@@ -15,6 +15,17 @@ module ApplicationHelper
       (link_to "Register", new_user_registration_path) +
       "<br>".html_safe +
       (link_to "Login", new_user_session_path)
+    end
+  end
+
+  # def sample_helper
+  #   content_tag(:div, "My content", class: "my-class")
+  # end
+
+  def source_heiper(layout_name)
+    if session[:source]
+      # greeting　＝　"Thanks for visiting me from #{session[:source]}"
+      content_tag(:p, "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout", class: "source-greeting")
     end
   end
 end
