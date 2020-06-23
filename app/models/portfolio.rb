5b837636acab360ca29fb5b59ef765b16d2b5237
tomokoichikawa <takator@yahoo.co.jp>
2020-06-23 21:13:40 +0900

    Integrated image background for app layout

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,7 +4,7 @@ class Portfolio < ApplicationRecord
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
 
   include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  # validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
     where(subtitle: "乃木坂4期生")
