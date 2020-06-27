fd8c4f6c9a1ffde92244779d8507b49144db88a5
tomokoichikawa <takator@yahoo.co.jp>
2020-06-27 19:26:38 +0900

    Finalized styles for tech news and updated nav width for app layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -55,6 +55,10 @@ module ApplicationHelper
         url: portfolios_path,
         title: 'Portfolio'
       },
+      {
+        url: tech_news_path,
+        title: 'Tech News'
+      }
     ]
   end
 
